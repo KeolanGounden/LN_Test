4d34195911efa2cd844059f727ac1eefabb18c4d
Keolan <keolan.gounden@gmail.com>
2026-01-23 19:12:21 +0200

    wip: fixed ng-select rendering

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -6,6 +6,7 @@ import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { ApiModule, Configuration } from './modules/ct-client';
 import { environment } from '../environment/environment';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { OVERLAY_DEFAULT_CONFIG } from '@angular/cdk/overlay';
 
 
 export const appConfig: ApplicationConfig = {
@@ -17,6 +18,9 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(
       ApiModule.forRoot(() => new Configuration({ basePath: environment.apiUrl }))
     ),
-    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+    { provide: OVERLAY_DEFAULT_CONFIG, useValue:{
+  usePopover: false,
+} }
 ]
 };
